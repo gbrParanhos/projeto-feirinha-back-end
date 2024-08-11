4d45c9e8e33875b3902d3d254cd8dc1c6c5bb9b1
gbrParanhos <gbrparanhos@gmail.com>
2024-08-11 13:49:11 -0300

    request get "/items" finished

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const items = [
 ]
 
 server.get('/items', (req, res) => {
+  let filteredItems = items;
+  if(req.query.type) filteredItems = items.filter(item => item.type === req.query.type.toLowerCase());
+  res.send(filteredItems);
 })
 
 server.get('/items/:id', (req, res) => {
@@ -14,9 +17,9 @@ server.get('/items/:id', (req, res) => {
 
 server.post('/items', (req, res) => {
   const {name, quantity, type} = req.body;
-  if (items.find(element => element.name === name)) return res.sendStatus(409);
+  if (items.find(item => item.name === name)) return res.sendStatus(409);
   if (Object.getOwnPropertyNames(req.body).length !== 3 || !name || !quantity || !type) return res.sendStatus(422);
-  const newItem = {id:items.length+1,...req.body};
+  const newItem = {id:items.length+1,...req.body,type:type.toLowerCase()};
   items.push(newItem);
   res.status(201).send(newItem);
 })
